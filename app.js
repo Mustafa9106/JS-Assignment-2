e5580c4622b9c7723df5435ddd6a551251f93b45
Muhammad Mustafa Khan <137421729+Mustafa9106@users.noreply.github.com>
2024-01-19 16:20:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var userName = prompt('What is your name?')
+alert('Greetings!'+ " " + userName + " " + 'Welcome to this page' )
+
+var length = prompt('Length of rectangle')
+var width = prompt('Width of rectangle')
+
+let area = length*width
+console.log('This area of rectangle is'+ " " + area);
